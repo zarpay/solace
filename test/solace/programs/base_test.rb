9bdcf267d1fa80ac941d9874beda259804be8dd8
Sebastian Scholl <sebscholl@gmail.com>
2025-07-21 16:23:46 +0700

    working on SplToken program model for CreateMint WIP

--- test/solace/programs/base_test.rb
+++ test/solace/programs/base_test.rb
@@ -3,18 +3,20 @@
 require 'test_helper'
 
 describe Solace::Programs::Base do
-  subject { described_class.new(connection: connection, program_id: program_id) }
-
+  let(:klass) { Solace::Programs::Base }
+  
   let(:connection) { Solace::Connection.new }
   let(:program_id) { '11111111111111111111111111111111' }
-
+  
   describe '#initialize' do
+    subject { klass.new(connection: connection, program_id: program_id) }
+    
     it 'assigns connection' do
-      expect(subject.connection).to eq(connection)
+      assert_equal subject.connection, connection
     end
 
     it 'assigns program_id' do
-      expect(subject.program_id).to eq(program_id)
+      assert_equal subject.program_id, program_id
     end
   end
 end
